43fa5b2c5511e3ed854177ed8001fbefeadfb1d6
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-03-14 17:42:10 +0000

    🎇:w1:p1

--- s/g.sh
+++ s/g.sh
@@ -3,7 +3,7 @@ echo "==================="
 echo ""
 echo "git add -all && git commit -m <addurshit>&& git push"
 git add --all 
-git commit -m "🎇:w1"
+git commit -m "🎇:w1:p1"
 git push --progress
 echo ""
 echo "=========PUSH DONE========="
