e9bbc6e1ee54f936da64e30527a6191ab25edc4d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-03-14 19:02:59 +0000

    🎇:w1:p1

--- 1w/t1/app/page.tsx
+++ 1w/t1/app/page.tsx
@@ -2,15 +2,25 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { headers } from "next/headers";
+
+// This setionis for importing the image 
+import booty from "../public/b.gif";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const headersList = headers();
+
   return (
     <main className={styles.main}>
       <h1>Sup Brah</h1>
       <Link href="/about">Go to About page</Link>
       <h2> Nose in her booty </h2>
+      <Image
+        src={booty}
+      />
+      ;
       <h3>
         <code>This is the main </code>
       </h3>{" "}
