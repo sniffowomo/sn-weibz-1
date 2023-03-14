d56988bd7f0f3b13a9fb21689da187abe1b09eba
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-03-14 16:52:26 +0000

    🎇:w1

--- 1w/t1/app/page.tsx
+++ 1w/t1/app/page.tsx
@@ -1,19 +1,20 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-    
-    <h1>Sup Brah</h1>
-    <h2> Nose in her booty   </h2>
-    <h3><code>This is the main </code></h3>
-    <h4><i> Drink her sweet woman juice </i></h4>
-  
-
+      <h1>Sup Brah</h1>
+      <h2> Nose in her booty </h2>
+      <h3>
+        <code>This is the main </code>
+      </h3>{" "}
+      <h4>
+        <i> Drink her sweet woman juice </i>
+      </h4>
     </main>
-  )
+  );
 }
