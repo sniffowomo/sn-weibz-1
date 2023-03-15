581e8ace503ed5858d20fb5d06f6a7bb443b9bfa
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-03-15 18:34:34 +0000

    🎇:w1:p1

--- 1w/t1/app/about/layout.tsx
+++ 1w/t1/app/about/layout.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import styles from "./styles.module.css";
 
+export const metadata = {
+  title: "AboutLiq",
+  description: "AboutStink",
+  colorScheme: "dark",
+  creator: "bootysucker",
+  type: "womanstinkfetish",
+  bookmarks: ["NoseInHerBooty"],
+  category: "PantyFetish",
+};
+
 export default function RootLayout({
   children,
 }: {
